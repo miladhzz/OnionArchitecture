d8d937cec8fb6434a4c02c103d9405dd80b96c45
miladhzz <miladhzz@gmail.com>
2021-05-07 11:03:17 -0700

    add carType application

---
+++ CF.Application.Contracts/CarType/CarTypeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CF.Application.Contracts.CarType
+{
+    public class CarTypeViewModel
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public string CreateTime { get; private set; }
+    }
+}
---
+++ CF.Application.Contracts/CarType/ICarTypeApplication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CF.Application.Contracts.CarType
+{
+    public interface ICarTypeApplication
+    {
+        CarTypeViewModel Get(int id);
+    }
+}
